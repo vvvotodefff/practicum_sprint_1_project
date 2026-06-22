6c996f4efede87cbae8c64ade0fd79b421d319be
Fedotov.A <User@WIN-C2B77R727EV>
2026-06-22 16:38:36 +0300

    Реализация эндпоинтов REST API

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Net;
-using MyWebApiProject;
 using ProjectWork.Services;
 using ProjectWork.Models;
 
@@ -21,6 +19,41 @@ namespace ProjectWork.Controllers
         public ActionResult<IEnumerable<Event>> GetEvents()
         {
             return Ok(_eventService.GetEvents());
+        }
+
+        [HttpGet("{id:guid}")]
+        public ActionResult<Event> GetEventById(Guid id)
+        {
+            var eventItem = _eventService.GetEventById(id);
+
+            if (eventItem is null)
+                return NotFound();
+
+            return Ok(eventItem);
+        }
+
+        [HttpPost]
+        public ActionResult<Event> CreateEvent(Event eventItem)
+        {
+            _eventService.AddEvent(eventItem);
+            return CreatedAtAction(nameof(GetEventById), new { id = eventItem.Id }, eventItem);
+        }
+
+
+        [HttpPut("{id:guid}")]
+        public IActionResult UpdateEvent(Guid id, Event eventItem)
+        {
+            if (!_eventService.UpdateEvent(id, eventItem))
+                return NotFound();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult DeleteEvent(Guid id)
+        {
+            if (!_eventService.DeleteEvent(id))
+                return NotFound();
+            return NoContent();
         }
 
     }
